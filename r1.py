096283b459befd26e00db48255cc1f92568222ba
saadbinabad <saadabad@gsoftconsulting.com>
2025-01-22 00:23:12 +0500

    Add files via upload

---
+++ r1.py
@@ -0,0 +1,8 @@
+from reviews_dashboard import display_reviews_dashboard
+
+def main():
+    """Main function to run the application."""
+    display_reviews_dashboard()
+
+if __name__ == "__main__":
+    main()
